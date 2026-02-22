0d7dc6fde283403670928b6581b4e5c565112526
Sourin De <sdgamer8263@gmail.com>
2026-02-22 19:12:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -142,7 +142,8 @@ main_menu() {
         1) install_rdp_full ;;
         2) browsers_menu ;;
         3) apt update -y && apt upgrade -y && apt autoremove -y; success_msg "System Clean" ;;
-        0) exit 0 ;;
+        0) bash <(curl -sL https://raw.githubusercontent.com/sdgamer8263-sketch/tooler/main/run.sh)
+        exit 0 ;;
         *) main_menu ;;
     esac
     main_menu
